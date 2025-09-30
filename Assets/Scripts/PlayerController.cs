ae71c51bfd8bf2859723131f2c1a5754b76c3f4f
pipelin123 <lopezlondono3@gmail.com>
2025-09-30 14:27:17 -0500

    Collectables, Score, Spikes
    Aquí se añadió la interacción con los coleccionables, los pinchos y el score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f; //variable para guardar la velocidad
+    public int score = 0;
+    public bool hasKey = false;
+    public bool hasSpike = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,5 +25,41 @@ public class PlayerController : MonoBehaviour
 
         transform.Translate(direction * speed * Time.deltaTime);
 
+    }
+    //funcion especial que se ejecuta cuando se toca a otro objeto que tiene un collider en modo //trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Print("prueba");
+        if (other.CompareTag("collectable"))
+        {
+            score = score + 1;
+            
+
+            Destroy(other.gameObject);
+            Debug.Log("Collected!!!");
+            Debug.Log("Score: " + score);
+
+        }
+        if (other.CompareTag("Key"))
+        {
+            hasKey = true;
+            Debug.Log("has recolectado la llave!");
+            Destroy(other.gameObject);
+            
+        }
+        if (other.CompareTag("Spike"))
+        {
+            hasSpike = true;
+            Debug.Log("has recolectado la llave!");
+            Destroy(gameObject);
+        }
+        
+        
+        //codicion de victoria
+        if (score >= 3 || hasKey && !hasSpike)  //solo en un booleano si se pregunta sin nada es verdadero, y con un signo de admiracion al principio es falso
+        {
+            Debug.Log("Has ganado. Tienes suficientes puntos, la llave y no has tocado los pinchos");
+            
+        }
     }
 }
